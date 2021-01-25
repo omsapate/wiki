0fa833c1fff8fa444a2dc6a43042e9eac7442229
omsapate <omsapate@outlook.com>
2021-01-25 12:06:51 +0530

    create new page implementation

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,6 @@ appname = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:page_name>",views.entry_page,name="entry"),
-    path("searchpage", views.search, name="searchpage")
+    path("searchpage", views.search, name="searchpage"),
+    path("newpage", views.newpage, name="newpage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,6 +7,10 @@ from . import util
 class SearchForm(forms.Form):
     query = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
 
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title",required=True, widget=forms.TextInput(attrs={'placeholder':'Enter Title'}))
+    textArea = forms.CharField(label='',required=False,widget=forms.Textarea(attrs={'placeholder': 'Enter Page Content'}))
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
@@ -22,7 +26,8 @@ def entry_page(request, page_name):
         })
     else: 
         return render(request,"encyclopedia/error_page.html",{
-            "content" : page_name,
+            'message' : "Page does not exist",
+            'title' : "Page does not exist",
             "form": SearchForm(),
         })
 
@@ -38,14 +43,35 @@ def search(request):
             # for title in data:
             #     if query in 
 
-            if len(files)>1:
+            if files[0].lower() == query and len(files)==1:
+                return entry_page(request, query)
+
+
+            elif len(files)>=1:
                 return render(request, 'encyclopedia/searchpage.html',{"entries":files,"form":SearchForm()})
 
-            elif len(files)==1:
-                return entry_page(request, query)
+def newpage(request):
+
+    if request.method == 'POST':
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            data  = util.list_entries()
             
+            if title in data:
+                return render(request, "encyclopedia/error_page.html",
+                {
+                    "title" : "Error",
+                    "message": "Title already exist! Try New one" ,
+                    "form": SearchForm()                
+                }
+                )
+            else:
+                textArea = form.cleaned_data['textArea']
+                util.save_entry(title, textArea)
 
+                return entry_page(request, title)
 
+    return render(request, "encyclopedia/newpage.html",{"form": SearchForm(), "Newpageform": NewPageForm()})
+                
 
-            
-    
